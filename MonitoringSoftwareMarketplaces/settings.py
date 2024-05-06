e35e11ae2ed2eb1f553643ad0cdc4fe78d39e460
maciasj <jmaciasrojas@gmail.com>
2024-05-06 18:58:03 +0200

    Añadidas features de seguridad para la conexion de la base de datos

--- MonitoringSoftwareMarketplaces/settings.py
+++ MonitoringSoftwareMarketplaces/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'MonitoringSoftwareMarketplaces.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SMM_database',
-        'USER': 'root',
-        'PASSWORD': 'Marketplace24',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
 
     }
 }
